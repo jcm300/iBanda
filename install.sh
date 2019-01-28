c2164fad68fa8ae360c1d35bf1098eb9f450c9e2
jcm300 <zeclmartins@gmail.com>
2019-01-28 17:11:18 +0000

    minor fix in script

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ if [ $# -eq 1 ]; then
         #Clone rep
         git clone https://github.com/jcm300/iBanda.git
 
-        echo "Change the url in variable app.locals.url in app.js and change the private and public key in the folder auth. When done press some letter to continue."
+        echo "Change the url in variable app.locals.url and root password in app.js, change the private and public key in the folder auth, change the port in bin/www. When done press some letter to continue."
         read -n 1 -p "Continue?";
 
         #start mongoDB
